e30a8e959de5340abeed2683f585bb95bfa74a7b
Daniel Mrkoci <daniel@wiklunds.nu>
2022-11-08 18:44:46 +0100

    added comments on proceding forward

--- src/components/issues/IssueCard.js
+++ src/components/issues/IssueCard.js
@@ -1,5 +1,7 @@
 import React from 'react'
 
+// Need to create cards with styling
+
 const IssueCard = () => {
   return (
     <div>IssueCard</div>
--- src/views/Issues.js
+++ src/views/Issues.js
@@ -2,6 +2,7 @@ import React from 'react'
 import Navbar from '../components/navbar/Navbar'
 import '../views/styles/Issues.css'
 
+// Need to create Issue card with styling and create a grid to display in Issues.js
 const Issues = () => {
   return (
     <>
