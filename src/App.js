ffd2ecf7d39e2e8a7e89e5e0e23ba7806088e59f
Daniel Mrkoci <daniel@wiklunds.nu>
2022-11-08 18:42:38 +0100

    Created Form view and added styling

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import { Routes, Route } from 'react-router-dom';
 import Form from './views/Form';
 import Issues from './views/Issues'
+import './App.css'
 
 function App() {
   return (
     <div className='App'>
       <Routes>
+        <Route path='/' element={ <Form/> } />
         <Route path='/form' element={ <Form />} />
         <Route path='/issues' element={ <Issues />} />
       </Routes>
--- src/views/Form.js
+++ src/views/Form.js
@@ -6,7 +6,17 @@ const Form = () => {
   return (
     <>
     <Navbar/>
-    <div>Form</div>
+    <form className='container'>
+      <div className='d-flex'>
+      <label>Email:</label>
+      <input type="email" id='email' name='email' className='form-email form-control' placeholder='Ex. John.Doe@mail.com'></input>
+      <label>Subject:</label>
+      <input type="text" id='subject' name='subject' className='form-subject form-control' placeholder='Write an issue...'></input>
+      <label>Message:</label>
+      <textarea type="textarea" id='message' name='message' className='form-message form-control' placeholder='Explain issue in greater detail...'></textarea>
+      <button id='btn-submit' name='btn-submit' className='btn btn-primary'>Submit</button>
+      </div>
+    </form>
     </>
   )
 }
