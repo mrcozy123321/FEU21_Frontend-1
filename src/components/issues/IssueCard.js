dfb05f5fe3019e215dc422776839d92fe5a056a1
Daniel Mrkoci <daniel@wiklunds.nu>
2022-11-09 01:31:18 +0100

    created issuecard and styling

--- src/components/issues/IssueCard.js
+++ src/components/issues/IssueCard.js
@@ -1,10 +1,24 @@
 import React from 'react'
-
+import './IssueCard.css'
 // Need to create cards with styling
 
 const IssueCard = () => {
   return (
-    <div>IssueCard</div>
+    <div className='issue'>
+      <div className='container'>
+        <div className='issuecard'>
+          <div className='issuecard-header'>
+            <p id='issuecard-email' name='issuecard-email' className='issuecard-email'>John.Doe@mail.com</p>
+            <div className='issuecard-status-container'>
+              <p id='issuecard-status' name='issuecard-status'>Status</p>
+              <div id='issuecard-status-ball' name='issuecard-status-ball' className='issuecard-status-ball issuecard-status-complete'></div>
+            </div>
+          </div>
+          <p id='issuecard-subject' name='issuecard-subject' className='issuecard-subject'>Subject for something random to talk about</p>
+          <p id='issuecard-message' name='issuecard-message' className='issuecard-message'>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Nihil, natus. Lorem ipsum dolor sit, amet consectetur adipisicing elit. Nihil, natus. Lorem ipsum dolor sit, amet consectetur adipisicing elit. Nihil, natus. Lorem ipsum dolor sit, amet consectetur adipisicing elit. Nihil, natus.</p>
+        </div>
+      </div>
+    </div>
   )
 }
 
--- src/views/Issues.js
+++ src/views/Issues.js
@@ -1,13 +1,16 @@
 import React from 'react'
 import Navbar from '../components/navbar/Navbar'
 import '../views/styles/Issues.css'
+import IssueCard from '../components/issues/IssueCard'
 
 // Need to create Issue card with styling and create a grid to display in Issues.js
 const Issues = () => {
   return (
     <>
     <Navbar/>
-    <div>Issues</div>
+    <div>
+      <IssueCard></IssueCard>
+    </div>
     </>
   )
 }
